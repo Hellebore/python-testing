b4a8309bf7b5bb028005d5368cd05210e1e38231
Hellebore <nthapen@gmail.com>
2020-03-31 20:58:47 +0100

    move test

--- test.py
+++ test.py
@@ -20,3 +20,25 @@ def function():
 def func2():
     if False:
         pass
+
+
+
+def move_assign(cond1, cond2):
+  a_var = 10
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  if cond1:
+    c_var = 30
+    if cond2:
+      print(a_var)
+    print(a_var)
