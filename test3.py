faa2291086c82ff7f2402742a60effe7fa8422de
Hellebore <nthapen@gmail.com>
2020-04-22 13:27:10 +0100

    add another test

---
+++ test3.py
@@ -0,0 +1,69 @@
+
+
+
+def move_assign(cond1, cond2):
+  a_var = 10
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+
+
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+
+
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  if cond1:
+    c_var = 30
+    if cond2:
+      print(a_var)
+    print(a_var)
+
+def while_to_for():
+  i = 0
+  while i < 10:
+    do_x()
+    i += 1
+
+
+
