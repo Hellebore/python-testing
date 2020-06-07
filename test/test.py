d500fb6c90fab2256ab41da4693fc8486e6bf4f9
Hellebore <nthapen@gmail.com>
2020-06-07 13:33:49 +0100

    testing

---
+++ test/test.py
@@ -0,0 +1,50 @@
+# this is a test script
+
+
+
+if True:
+    pass
+
+
+def function():
+
+    if False:
+        pass
+
+    if x == None:
+        do_x()
+
+    return None
+    do_x()
+
+
+def func2():
+    if False:
+        pass
+
+
+
+def move_assign(cond1, cond2):
+  a_var = 10
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  b_var = 20
+  if cond1:
+    c_var = 30
+    if cond2:
+      print(a_var)
+    print(a_var)
+
+
+def func():
+    if False:
+        pass
