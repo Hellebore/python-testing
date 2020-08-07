338ef76deb764552466dd289ba3d4478e731ae0f
Hellebore <nthapen@gmail.com>
2020-08-07 14:56:37 +0100

    new example

--- test.py
+++ test.py
@@ -42,3 +42,19 @@ def move_assign(cond1, cond2):
     if cond2:
       print(a_var)
     print(a_var)
+
+
+def remove_escapes(text: str) -> str:
+  counter = 0
+  res = ""
+  is_escaped = False
+  while counter < len(text):
+    if is_escaped:
+      res += text[counter]
+      is_escaped = False
+    elif text[counter] == "\\":
+      is_escaped = True
+    else:
+      res += text[counter]
+    counter += 1
+  return res
