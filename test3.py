07d16edacad420a0c468d04f19e82b5dab128146
Sourcery AI <>
2020-10-27 16:29:57 +0000

    'Refactored by Sourcery'

--- test3.py
+++ test3.py
@@ -2,7 +2,6 @@
 
 
 def move_assign(cond1, cond2):
-  a_var = 10
   b_var = 20
   b_var = 20
   b_var = 20
@@ -55,15 +54,14 @@ def move_assign(cond1, cond2):
   b_var = 20
   if cond1:
     c_var = 30
+    a_var = 10
     if cond2:
       print(a_var)
     print(a_var)
 
 def while_to_for():
-  i = 0
-  while i < 10:
+  for _ in range(10):
     do_x()
-    i += 1
 
 
 
